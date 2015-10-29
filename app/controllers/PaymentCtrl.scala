5a0f78e20beaa88e5673c9d7efca2db05972a513
topy <ppp21spider@sina.com>
2015-10-29 22:49:44 +0800

    MOD: 完成统一下单接口

--- app/controllers/PaymentCtrl.scala
+++ app/controllers/PaymentCtrl.scala
@@ -2,46 +2,70 @@ package controllers
 
 import javax.inject.Singleton
 
-import core.api.OrderAPI
 import core.misc.HanseResult
 import core.misc.Implicits._
 import core.service.PaymentService
-import org.bson.types.ObjectId
 import play.api.mvc.{ Action, Controller }
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
-import scala.xml.Elem
 
 @Singleton
 class PaymentCtrl extends Controller {
 
   def createPayments(orderId: String) = Action.async(
     request => {
-      Future {
-        val ret = for {
-          body <- request.body.asJson
-          userId = request.headers.get("UserId") map (_.toLong)
-          name = (body \ "name").asOpt[String].getOrElse("")
-          ip = (body \ "ip").asOpt[String].getOrElse("")
-          tradeType = (body \ "tradeType").asOpt[String].getOrElse("")
-          vendor = (body \ "vendor").asOpt[String].getOrElse("")
+      val ret = for {
+        body <- request.body.asJson
+        userId = request.headers.get("UserId") map (_.toLong)
+        name = (body \ "name").asOpt[String].getOrElse("")
+        ip = (body \ "ip").asOpt[String].getOrElse("")
+        tradeType = (body \ "tradeType").asOpt[String].getOrElse("")
+        vendor = (body \ "vendor").asOpt[String].getOrElse("")
+      } yield {
+        for {
+          //orderValue <- OrderAPI.getOrder(new ObjectId(orderId))
+          wcResponse <- PaymentService.unifiedOrder(
+            Map("out_trade_no" -> orderId,
+              "attach" -> "微信支付", "spbill_create_ip" -> ip, "body" -> "Onetemp",
+              "trade_type" -> tradeType, "total_fee" -> 100 * 100))
         } yield {
-          for {
-            orderValue <- OrderAPI.getOrder(new ObjectId(orderId))
-            wcResponse <- PaymentService.unifiedOrder(
-              Map("out_trade_no" -> orderId, "openid" -> userId.getOrElse(0).toString,
-                "attach" -> "微信支付", "ip" -> ip, "body" -> name,
-                "trade_type" -> tradeType, "total_fee" -> orderValue.getTotalPrice * 100))
-          } yield {
-            val node = wcResponse.asInstanceOf[Elem]
-
-            null
-          }
+          //val node = wcResponse.asInstanceOf[Elem]
+          val str = new String(wcResponse.bodyAsBytes, "UTF8")
+          Ok(str)
         }
-        ret getOrElse Future(HanseResult.unprocessable())
       }
-      null
+      ret getOrElse Future {
+        HanseResult.unprocessable()
+      }
+    }
+  )
+
+  def wechatCallback() = Action.async(
+    request => {
+      val ret = for {
+        body <- request.body.asJson
+        userId = request.headers.get("UserId") map (_.toLong)
+        name = (body \ "name").asOpt[String].getOrElse("")
+        ip = (body \ "ip").asOpt[String].getOrElse("")
+        tradeType = (body \ "tradeType").asOpt[String].getOrElse("")
+        vendor = (body \ "vendor").asOpt[String].getOrElse("")
+      } yield {
+        //          for {
+        //            orderValue <- OrderAPI.getOrder(new ObjectId(orderId))
+        //            wcResponse <- PaymentService.unifiedOrder(
+        //              Map("out_trade_no" -> orderId, "openid" -> userId.getOrElse(0).toString,
+        //                "attach" -> "微信支付", "ip" -> ip, "body" -> "One temp",
+        //                "trade_type" -> tradeType, "total_fee" -> 100 * 100))
+        //          } yield {
+        //            //val node = wcResponse.asInstanceOf[Elem]
+        //            HanseResult.ok(RetCode.OK, None, Some(wcResponse))
+        //          }
+        null
+      }
+      ret getOrElse Future {
+        HanseResult.unprocessable()
+      }
     }
   )
 
--- app/core/service/PaymentService.scala
+++ app/core/service/PaymentService.scala
@@ -1,36 +1,23 @@
 package core.service
 
-import com.typesafe.config.ConfigFactory
-import core.misc.Utils
-import play.api.Configuration
+import core.misc.{ Global, Utils }
 import play.api.Play.current
-import scala.concurrent.ExecutionContext.Implicits.global
 import play.api.libs.ws.WS
-
-import scala.concurrent.Future
 
 /**
  * Created by topy on 2015/10/22.
  */
 object PaymentService {
 
-  lazy val config = Configuration(ConfigFactory.load())
+  lazy val config = Global.conf
   val accountInfo = {
-    val appid = config.getString("wechatpay.appid").get
-    val mchid = config.getString("wechatpay.mchid").get
+    val appid = config.getString("wechatpay.appid").getOrElse("")
+    val mchid = config.getString("wechatpay.mchid").getOrElse("")
     Map("appid" -> appid, "mch_id" -> mchid)
   }
 
-  val appsecret = config.getString("wechatpay.appsecret").get
+  val apisecret = config.getString("wechatpay.apisecret").getOrElse("")
 
-  /**
-   *
-   * application/xml
-   * @param url
-   * @param contentType
-   * @param body
-   * @return
-   */
   //  def sendPostRequest(url: String, contentType: String, body: String) = {
   //    val client = HttpClientBuilder.create().build()
   //    val post = new HttpPost(url)
@@ -45,26 +32,33 @@ object PaymentService {
    *
    */
   def unifiedOrder(content: Map[String, String]) = {
-    Future {
-      val url = "https://api.mch.weixin.qq.com/pay/unifiedorder"
-      val notify_url = "http://182.92.168.171:11219/app/payment-webhook/wechat"
+    val url = "https://api.mch.weixin.qq.com/pay/unifiedorder"
+    val notify_url = "http://182.92.168.171:11219/payment-webhook/wechat"
 
-      val callBack = Map("notify_url" -> notify_url)
-      val randomStr = Map("nonce_str" -> Utils.nonceStr())
+    val callBack = Map("notify_url" -> notify_url)
+    val randomStr = Map("nonce_str" -> Utils.nonceStr())
 
-      val params = content ++ accountInfo ++ callBack ++ randomStr
-      val sign = Map("sign" -> genSign(params))
-      val resultParams = params ++ sign
-      val body = Utils.addChildrenToXML(<xml></xml>, resultParams).toString()
-      val ret = WS.url(url).withHeaders("Content-Type" -> "application/xml; charset=utf-8").post(body)
-      ret
-    }
+    val params = content ++ accountInfo ++ callBack ++ randomStr
+    val sign = Map("sign" -> genSign(params))
+    val resultParams = params ++ sign
+    val body = Utils.addChildrenToXML(<xml></xml>, resultParams)
+    val ret = WS.url(url)
+      .withHeaders("Content-Type" -> "text/xml; charset=utf-8")
+      .withRequestTimeout(30000)
+      .post(body.toString)
+    ret
+    //    val rr = ret.map {
+    //      response =>
+    //        val u = response.body
+    //      //response.xml \ "message"
+    //    }
+    //    rr
   }
 
   def genSign(content: Map[String, String]) = {
     val stringA = (for ((k, v) <- content.toList.sorted) yield s"$k=$v").mkString("&")
-    val stringSignTemp = stringA + "&key=" + appsecret
-    val sign = Utils.MD5(stringSignTemp).toUpperCase()
+    val stringSignTemp = stringA + "&key=" + apisecret
+    val sign = Utils.MD5(stringSignTemp).toUpperCase
     sign
   }
 
