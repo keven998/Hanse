1039669b48f4fc05e50535aec1d6f64591845516
topy <ppp21spider@sina.com>
2015-12-31 18:03:42 +0800

    MOD：添加申请成为商家

--- app/controllers/SellerCtrl.scala
+++ app/controllers/SellerCtrl.scala
@@ -7,10 +7,13 @@ import com.lvxingpai.yunkai.UserInfoProp
 import core.api.SellerAPI
 import core.formatter.marketplace.seller.SellerFormatter
 import core.misc.HanseResult
+import core.misc.Implicits.PhoneNumberTemp
+import org.apache.commons.lang.StringUtils
 import play.api.Configuration
 import play.api.mvc.{ Action, Controller }
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 /**
  * Created by topy on 2015/11/3.
@@ -36,6 +39,23 @@ class SellerCtrl @Inject() (@Named("default") configuration: Configuration, data
           HanseResult.notFound(Some(s"Seller not found. sellId is $id"))
       }
       ret
+    }
+  )
+
+  def becomeSeller() = Action.async(
+    request => {
+      val ret = for {
+        body <- request.body.asJson
+        userId <- (body \ "userId").asOpt[Long]
+        memo <- (body \ "memo").asOpt[String] orElse Some(StringUtils.EMPTY)
+        email <- (body \ "email").asOpt[String] orElse Some(StringUtils.EMPTY)
+        tel <- (body \ "tel").asOpt[PhoneNumberTemp]
+      } yield {
+        Future(HanseResult.ok())
+      }
+      ret.getOrElse(Future {
+        HanseResult.unprocessable()
+      })
     }
   )
 
