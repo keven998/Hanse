8bc2871c4bf714b7b75b10ee2193b9b735361c0f
topy <ppp21spider@sina.com>
2016-01-15 11:46:23 +0800

    MOD:添加店铺信息

--- app/core/formatter/marketplace/seller/SellerFormatter.scala
+++ app/core/formatter/marketplace/seller/SellerFormatter.scala
@@ -4,9 +4,11 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.module.SimpleModule
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.lvxingpai.model.account.UserInfo
+import com.lvxingpai.model.geo.GeoEntity
 import com.lvxingpai.model.marketplace.seller.Seller
 import com.lvxingpai.model.misc.{ ImageItem, RichText, PhoneNumber }
 import core.formatter.BaseFormatter
+import core.formatter.geo.SimpleLocalitySerializer
 import core.formatter.misc.{ ImageItemSerializer, RichTextSerializer, PhoneNumberSerializer }
 import core.formatter.user.UserSerializer
 
@@ -23,6 +25,7 @@ class SellerFormatter extends BaseFormatter {
     module.addSerializer(classOf[UserInfo], new UserSerializer)
     module.addSerializer(classOf[RichText], new RichTextSerializer)
     module.addSerializer(classOf[PhoneNumber], new PhoneNumberSerializer)
+    module.addSerializer(classOf[GeoEntity], new SimpleLocalitySerializer)
     mapper.registerModule(module)
     mapper
   }
--- app/core/formatter/marketplace/seller/SellerSerializer.scala
+++ app/core/formatter/marketplace/seller/SellerSerializer.scala
@@ -46,7 +46,11 @@ class SellerSerializer extends JsonSerializer[Seller] {
     // 服务标签
     gen.writeFieldName("services")
     gen.writeStartArray()
-    Option(seller.services) map (_.toSeq) getOrElse Seq() foreach (gen writeString _)
+    Option(seller.services) map (_.toSeq) getOrElse Seq() map (_ match {
+      case "language" => "语言帮助"
+      case "plan" => "行程规划"
+      case "consult" => "当地咨询"
+    }) foreach (gen writeString _)
     gen.writeEndArray()
 
     // 服务区域，可以是国家，也可以是目的地
